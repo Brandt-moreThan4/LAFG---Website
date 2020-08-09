dac1fe1e45b714fdd4df489ddfc82ef14b7384a5
Brandt Green <153144Green@chsbr.net>
2020-08-08 22:58:13 -0500

    Changed admin url

--- LAFG/LAFG/urls.py
+++ LAFG/LAFG/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('manage/', admin.site.urls),
     path('', include('lafg_site.urls', namespace='lafg_site'))
 
 ]
