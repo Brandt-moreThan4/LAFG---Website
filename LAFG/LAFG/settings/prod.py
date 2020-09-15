47588a93709094c543596cad415b7b22f5820325
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2020-09-15 13:48:55 -0500

    add domain to settings.

--- LAFG/LAFG/settings/prod.py
+++ LAFG/LAFG/settings/prod.py
@@ -5,4 +5,4 @@ DEBUG = False
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/153144green/LAFG---Website/static'
 
-ALLOWED_HOSTS = ['153144green.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.usfocusgroups.org']
