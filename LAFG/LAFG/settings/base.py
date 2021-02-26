b4e2bf6529ca1ee0117d35c1e2585a0cd536e57c
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2021-02-26 14:07:53 -0600

    Removed Django wording from admin

--- LAFG/LAFG/settings/base.py
+++ LAFG/LAFG/settings/base.py
@@ -55,16 +55,16 @@ ROOT_URLCONF = 'LAFG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + '\\templates'],
         'APP_DIRS': True,
-        'OPTIONS': {
+        'OPTIONS': {    
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-        },
+        },  
     },
 ]
 
