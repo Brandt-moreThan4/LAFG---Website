bfec99a42c76b6e7922bce6b7f704ea2b3a198a7
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2021-03-07 16:57:59 -0600

    Basic survey structure set up

--- LAFG/survey/urls.py
+++ LAFG/survey/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 app_name = 'survey'
 
 urlpatterns = [
-    path('test_case/', views.test_case, name='test_case'),
+    path('test_case/<int:page_num>/', views.test_case, name='test_case'),
     ]
 
--- LAFG/survey/views.py
+++ LAFG/survey/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator
 
 
-print('http://127.0.0.1:8000/survey/test_case')
+print('http://127.0.0.1:8000/survey/test_case/1')
 
 
-@login_required
-def test_case(request):
+
+def test_case(request, page_num):
     """Renders home/ about page"""
 
-    return render(request, 'survey/test_case.html')
+    if page_num <1:
+        page_num = 1
+    elif page_num > 2:
+        page_num = 2
+
+
+    template_path = 'survey/test_case' + str(page_num) + '.html'
+
+    return render(request, template_path)
+
+
+
+#Password needed
+# def test_case(request):
+#     """Renders home/ about page"""
+
+#     if request.method == 'POST'
+
+#     return render(request, 'survey/test_case.html')
+
