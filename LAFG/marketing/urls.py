597f399b78a573b0ad036357ab8ca46ee43a4244
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2021-05-28 18:04:05 -0500

    usfg coming in hot

--- LAFG/marketing/urls.py
+++ LAFG/marketing/urls.py
@@ -5,6 +5,10 @@ from django.contrib.auth import views as auth_views
 app_name = 'marketing'
 
 urlpatterns = [
-    # path('contact/', views.contact, name='contact' ),
+    path('contact/', views.contact, name='contact' ),
+    path('faqs/', views.faqs, name='faqs' ),
+    path('focus-groups/', views.focus_groups, name='focus_groups' ),
+    path('demonstratives/', views.demonstratives, name='demonstratives'),
+    path('surveys/', views.surveys, name='surveys' ),
     path('', views.home, name='home' ),
     ]
